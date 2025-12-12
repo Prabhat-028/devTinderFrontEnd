aaef1561bd8c4ac9e6443400306312097020b160
Prabhat020 <kumar.prabhat23343@jeckukas.org.in>
2025-12-13 00:12:56 +0530

    creating the fronted of devtinder and adding daisyUi

---
+++ devTinderFrontend/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+	return (
+        <>
+            <h1 className="bg-green-200 text-white font-bold underline p-4">
+                Tailwind is Working!
+            </h1>
+            <button className="btn btn-primary">Test Button</button>
+        </>
+    );
+}
