e3633e0b9e08db5641a40b15e98863d8882cefdb
Prabhat020 <kumar.prabhat23343@jeckukas.org.in>
2025-12-26 17:11:58 +0530

    renaming the application

--- devTinderFrontend/src/assets/NavBar.jsx
+++ devTinderFrontend/src/assets/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
         <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
                 <Link to="/" className="btn btn-ghost text-xl">
-                    DEVTINDER
+                    MeetWith.dev
                 </Link>
             </div>
 
